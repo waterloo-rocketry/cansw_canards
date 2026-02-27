41ef7ee88d1a5ce717c83dc8dad4c7b5614b0a26
Shiming He <eshiminghe@gmail.com>
2026-02-27 21:24:25 +0000

    New imu handler header

--- src/application/imu_handler/imu_handler.h
+++ src/application/imu_handler/imu_handler.h
@@ -40,8 +40,9 @@ uint32_t imu_handler_get_status(void);
 
 /**
  * @brief Reports all IMU data based on request from outside tasks
- * @return all IMU data
+ * @param all_imu_data pointer to load the imu data on to
+ * @return Status of retreval
  */
-estimator_all_imus_input_t imu_handler_get_data(void);
+w_status_t imu_handler_get_data(estimator_all_imus_input_t *all_imu_data);
 
 #endif // IMU_HANDLER_H
