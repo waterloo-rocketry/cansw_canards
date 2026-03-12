8e376bfba9cbe138b0c2f071ff4175b8d1cec70b
Shiming He <eshiminghe@gmail.com>
2026-03-12 18:22:00 -0400

    add header for ADC files and add logging for init and self-test of gyro

--- src/drivers/ad_breakout_board/ads1219_stm32.h
+++ src/drivers/ad_breakout_board/ads1219_stm32.h
@@ -106,77 +106,104 @@ w_status_t ads1219_init(ads1219_handle_t *handle, i2c_bus_t bus, uint8_t addr);
 
 /**
  * @brief Issue a RESET command.
+ * @param handle handle to the ADC data
+ * @return status of function
  */
 w_status_t ads1219_reset(ads1219_handle_t *handle);
 
 /**
  * @brief Issue a START / SYNC command (begin conversion).
+ * @param handle handle to the ADC data
+ * @return status of function
  */
 w_status_t ads1219_start(ads1219_handle_t *handle);
 
 /**
  * @brief Issue a POWERDOWN command.
+ * @param handle handle to the ADC data
+ * @return status of function
  */
 w_status_t ads1219_powerdown(ads1219_handle_t *handle);
 
 /**
  * @brief Read the current gain setting from the config register.
+ * @param handle handle to the ADC data
  * @param[out] gain  ADS1219_GAIN_ONE or ADS1219_GAIN_FOUR
+ * @return status of function
  */
 w_status_t ads1219_get_gain(ads1219_handle_t *handle, uint8_t *gain);
 
 /**
  * @brief Set the gain.
+ * @param handle handle to the ADC data
  * @param[in] gain  ADS1219_GAIN_ONE or ADS1219_GAIN_FOUR
+ * @return status of function
  */
 w_status_t ads1219_set_gain(ads1219_handle_t *handle, uint8_t gain);
 
 /**
  * @brief Read the current voltage-reference setting.
+ * @param handle handle to the ADC data
  * @param[out] vref  ADS1219_VREF_INTERNAL or ADS1219_VREF_EXTERNAL
+ * @return status of function
  */
 w_status_t ads1219_get_vref(ads1219_handle_t *handle, uint8_t *vref);
 
 /**
  * @brief Set the voltage reference and ref voltages.
+ * @param handle handle to the ADC data
  * @param[in] vref    ADS1219_VREF_INTERNAL or ADS1219_VREF_EXTERNAL
  * @param[in] aref_n  Negative reference in mV (ignored for internal, 0 used)
  * @param[in] aref_p  Positive reference in mV (ignored for internal, 2048 used)
+ * @return status of function
  */
 w_status_t ads1219_set_vref(ads1219_handle_t *handle, uint8_t vref, float aref_n, float aref_p);
 
 /**
  * @brief Read the current data-rate setting.
+ * @param handle handle to the ADC data
  * @param[out] rate  0-3 (ADS1219_DATARATE_*)
+ * @return status of function
  */
 w_status_t ads1219_get_data_rate(ads1219_handle_t *handle, uint8_t *rate);
 
 /**
  * @brief Set the data rate.
+ * @param handle handle to the ADC data
  * @param[in] rate  0-3 (ADS1219_DATARATE_*)
+ * @return status of function
  */
 w_status_t ads1219_set_data_rate(ads1219_handle_t *handle, uint8_t rate);
 
 /**
  * @brief Read the current conversion-mode setting.
+ * @param handle handle to the ADC data
  * @param[out] mode  ADS1219_CM_SINGLE_SHOT or ADS1219_CM_CONTINUOUS
+ * @return status of function
  */
 w_status_t ads1219_get_conversion_mode(ads1219_handle_t *handle, uint8_t *mode);
 
 /**
  * @brief Set the conversion mode.
+ * @param handle handle to the ADC data
  * @param[in] mode  ADS1219_CM_SINGLE_SHOT or ADS1219_CM_CONTINUOUS
+ * @return status of function
  */
 w_status_t ads1219_set_conversion_mode(ads1219_handle_t *handle, uint8_t mode);
 
 /**
  * @brief Check whether a conversion result is ready.
+ * @param handle handle to the ADC data
  * @param[out] ready  true when a new result is available
+ * @return status of function
  */
 w_status_t ads1219_conversion_ready(ads1219_handle_t *handle, bool *ready);
 
 /**
- * TODO
+ * @brief Set the MUX Channel that is used
+ * @param handle handle to the ADC data
+ * @param channel the channel that is used (Macros defined above)
+ * @return status of function
  */
 w_status_t ads1219_set_channel(ads1219_handle_t *handle, uint8_t channel);
 
@@ -184,13 +211,16 @@ w_status_t ads1219_set_channel(ads1219_handle_t *handle, uint8_t channel);
  * @brief Convert a raw ADC count to millivolts.
  * @param[in]  handle    Device handle (carries reference voltages)
  * @param[in]  adc_count Raw signed count from a read function
- * @param[in]  gain      ADS1219_GAIN_ONE or ADS1219_GAIN_FOUR
  * @param[out] mv        Result in millivolts
+ * @return status of function
  */
 w_status_t ads1219_millivolts(ads1219_handle_t *handle, int32_t adc_count, float *mv);
 
 /**
- * @param data this is pointer to the data return which would be in terms of mv
+ * @brief Return the converted ADC voltage (millivolts)
+ * @param handle handle to the ADC data
+ * @param data this is pointer to the data return which would be in terms of mV
+ * @return status of function
  */
 w_status_t ads1219_get_millivolts(ads1219_handle_t *handle, float *data);
 
