0a5bd885a0740778adf59711fe8f39ed0cf5cc44
Shiming He <eshiminghe@gmail.com>
2026-03-11 23:09:34 -0400

    new headers for get data and self-test

--- src/drivers/ad_breakout_board/ADXRS649.h
+++ src/drivers/ad_breakout_board/ADXRS649.h
@@ -9,4 +9,16 @@
  */
 w_status_t adxrs649_init();
 
+/**
+ * @brief get the spin rate data from the ADXRS649 Gyro
+ * @param data is a pointer to where the data will be stored (deg/sec)
+ * @return the status of the get data function
+ */
+w_status_t adxrs649_get_gyro_data(float32_t *data);
+
+/**
+ * @brief perform the self-test on the ADXRS649
+ * @return the status of the self-test
+ */
+w_status_t adxrs649_self_test();
 #endif
