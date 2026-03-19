141d934cc19123c96dbe152a9cdf5d24c399caae
Shiming He <eshiminghe@gmail.com>
2026-03-18 20:57:32 -0400

    update header to include new data type

--- src/drivers/ad_breakout_board/ad_breakout_board.h
+++ src/drivers/ad_breakout_board/ad_breakout_board.h
@@ -1,7 +1,26 @@
-#include "rocketlib/include/common.h"
+
 
 #ifndef AD_BREAKOUT_BOARD_H
 #define AD_BREAKOUT_BOARD_H
+
+#include "rocketlib/include/common.h"
+#include "drivers/altimu-10/altimu-10.h"
+#include <stdint.h>
+
+typedef struct __attribute__((packed)) {
+	uint32_t timestamp_imu;
+	vector3d_t accelerometer; // gravities
+	float z_rate; // rad/sec
+    bool is_dead;
+} ad_breakout_board_mesurement_t;
+
+typedef struct __attribute__((packed)) {
+	uint32_t timestamp_imu;
+	altimu_raw_imu_data_t accelerometer; // gravities
+    // TODO: check if need the raw version of the data
+	float z_rate; // rad/sec
+} ad_breakout_board_raw_mesurement_t;
+
 
 /**
  * @brief initalize both the breakout board sensor drivers
@@ -16,8 +35,9 @@ void ad_breakout_board_task(void *argument);
 
 /**
  * @brief to read both the accelerometer and gyro data
- * @param data this is a pointer to which the sensor data is stored
+ * @param data this is a pointer to converted data
+ * @param raw_data pointer to raw data
  * @return the status of getting data
  */
-w_status_t ad_breakout_board_get_data(data_type *data);
+w_status_t ad_breakout_board_get_data(ad_breakout_board_mesurement_t *data, altimu_raw_imu_data_t *raw_data);
 #endif
