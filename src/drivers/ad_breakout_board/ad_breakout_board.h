a3dd2c6ca9a7954f8ab95e07e13e8cd246eac2ba
Shiming He <eshiminghe@gmail.com>
2026-03-19 18:07:00 -0400

    work on rtos task, get data, and data logging system

--- src/drivers/ad_breakout_board/ad_breakout_board.h
+++ src/drivers/ad_breakout_board/ad_breakout_board.h
@@ -3,23 +3,22 @@
 #ifndef AD_BREAKOUT_BOARD_H
 #define AD_BREAKOUT_BOARD_H
 
-#include "rocketlib/include/common.h"
 #include "drivers/altimu-10/altimu-10.h"
+#include "rocketlib/include/common.h"
 #include <stdint.h>
 
-typedef struct __attribute__((packed)) {
+typedef struct {
 	uint32_t timestamp_imu;
 	vector3d_t accelerometer; // TODO: based on the ADXL
 	float z_rate; // rad/sec
-    bool is_dead;
+	bool is_dead;
 } ad_breakout_board_mesurement_t;
 
-typedef struct __attribute__((packed)) {
+typedef struct {
 	altimu_raw_imu_data_t accelerometer; // TODO: based on the ADXL
-    // TODO: check if need the raw version of the data
+	// TODO: check if need the raw version of the data
 	int32_t z_rate; // rad/sec
 } ad_breakout_board_raw_mesurement_t;
-
 
 /**
  * @brief initalize both the breakout board sensor drivers
@@ -38,5 +37,6 @@ void ad_breakout_board_task(void *argument);
  * @param raw_data pointer to raw data
  * @return the status of getting data
  */
-w_status_t ad_breakout_board_get_data(ad_breakout_board_mesurement_t *data, altimu_raw_imu_data_t *raw_data);
+w_status_t ad_breakout_board_get_data(ad_breakout_board_mesurement_t *data,
+									  altimu_raw_imu_data_t *raw_data);
 #endif
