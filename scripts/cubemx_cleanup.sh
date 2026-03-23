3df46beadad496aee9b35437b0ab9d6858103718
celery6 <71736183+celery6@users.noreply.github.com>
2026-03-23 19:29:30 -0400

    move main.c back into src

--- scripts/cubemx_cleanup.sh
+++ scripts/cubemx_cleanup.sh
@@ -8,7 +8,8 @@ rm -rf "$DST/Middlewares"
 rm -rf "$DST/Startup"
 
 # move top-level files from src/ to dst/src (none of our source code is top-level, so this is safe)
-find "src" -maxdepth 1 -type f -exec mv {} "$DST/src/" \;
+# BUT keep main.c cuz its the only file we have USER-CODE in so dont let cubemx regen over it
+find "src" -maxdepth 1 -type f ! -name "main.c" -exec mv {} "$DST/src/" \;
 # move auto-gen folders from src/ to dst/
 mv -f Drivers $DST
 mv -f Middlewares $DST
