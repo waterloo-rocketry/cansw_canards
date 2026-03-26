ec4ff7ade8cd77f76bf71bc01ebd2d27000bc656
Shiming He <eshiminghe@gmail.com>
2026-03-26 17:03:57 -0400

    add init tests

--- tests/unit/adxl380_test.cpp
+++ tests/unit/adxl380_test.cpp
@@ -22,307 +22,155 @@ FAKE_VALUE_FUNC(w_status_t, adxl38x_soft_reset, adxl38x_dev_t *);
 FAKE_VALUE_FUNC(w_status_t, adxl38x_set_op_mode, adxl38x_dev_t *, adxl38x_op_mode_t);
 FAKE_VALUE_FUNC(w_status_t, adxl38x_set_range, adxl38x_dev_t *, adxl38x_range_t);
 FAKE_VALUE_FUNC(w_status_t, adxl38x_register_update_bits, adxl38x_dev_t *, uint8_t, uint8_t, uint8_t);
+FAKE_VALUE_FUNC(w_status_t, adxl38x_read_device_data, adxl38x_dev_t *, uint8_t, uint8_t, uint8_t);
 
 FAKE_VALUE_FUNC_VARARG(w_status_t, log_text, uint32_t, const char *, const char *, ...);
 }
 
-gpio_level_t global_gpio_value = GPIO_LEVEL_HIGH;
-w_status_t gpio_return_value = W_SUCCESS;
-w_status_t gpio_set_read(gpio_pin_t pin, gpio_level_t *level, uint32_t timeout) {
-    *level = global_gpio_value;
-    return gpio_return_value;
-}
-
-
-gpio_level_t global_ads1219_conversion_ready = false;
-w_status_t ads1219_conversion_ready_return = W_SUCCESS;
-w_status_t ads1219_set_conversion_ready(ads1219_handle_t *handle, bool *ready) {
-    *level = global_gpio_value;
-    return ads1219_conversion_ready_return;
-}
-
-float global_adc_output_mv = 0;
-w_status_t ads1219_set_millivolts_return = W_SUCCESS;
-w_status_t ads1219_set_millivolts(ads1219_handle_t *handle, const int32_t adc_count, float *mv) {
-    *mv = global_adc_output_mv;
-    return ads1219_set_millivolts_return;
-}
-
-class ADXRS649 : public ::testing::Test {
+class ADXL380 : public ::testing::Test {
 protected:
     void SetUp() override {
         RESET_FAKE(gpio_read);
         RESET_FAKE(gpio_write);
 
-        RESET_FAKE(ads1219_get_millivolts);
-        RESET_FAKE(ads1219_init);
-        RESET_FAKE(ads1219_set_channel);
-        RESET_FAKE(ads1219_set_conversion_mode);
-        RESET_FAKE(ads1219_set_gain);
-        RESET_FAKE(ads1219_set_vref);
-        RESET_FAKE(ads1219_start);
-        RESET_FAKE(ads1219_conversion_ready);
-        RESET_FAKE(ads1219_read_value);
-        RESET_FAKE(ads1219_millivolts);
-        RESET_FAKE(adxrs649_self_test);
+        RESET_FAKE(adxl38x_init);
+        RESET_FAKE(adxl38x_soft_reset);
+        RESET_FAKE(adxl38x_set_op_mode);
+        RESET_FAKE(adxl38x_set_range);
+        RESET_FAKE(adxl38x_register_update_bits);
+
+        RESET_FAKE(adxl38x_read_device_data);
 
         RESET_FAKE(log_text);
-        FFF_RESET_HISTORY();
-        global_gpio_value = GPIO_LEVEL_HIGH;
-        gpio_return_value = W_SUCCESS;  
-        global_ads1219_conversion_ready = false;
-        ads1219_conversion_ready_return = W_SUCCESS;  
-        global_adc_output_mv = 0;
-        ads1219_set_millivolts_return = W_SUCCESS;   
+        FFF_RESET_HISTORY(); 
     }
 
     void TearDown() override {}
 };
 
-TEST_F(ADXRS649, initSuccess){
+TEST_F(ADXL380, initSuccess){
 
     // set up function returns
-    ads1219_init_fake.return_val = W_SUCCESS;
+    adxl38x_init_fake.return_val = W_SUCCESS;
 
     // set up
-    ads1219_set_channel_fake.return_val = W_SUCCESS;
-    ads1219_set_conversion_mode_fake.return_val = W_SUCCESS;
-    ads1219_set_gain_fake.return_val = W_SUCCESS;
-    ads1219_set_data_rate_fake.return_val = W_SUCCESS;
-    ads1219_set_vref_fake.return_val = W_SUCCESS;
+    adxl38x_soft_reset_fake.return_val = W_SUCCESS;
+    adxl38x_set_op_mode_fake.return_val = W_SUCCESS;
+    adxl38x_set_range_fake.return_val = W_SUCCESS;
+    adxl38x_register_update_bits_fake.return_val = W_SUCCESS;
 
-    // self-test
-    adxrs649_self_test_fake.return_val = W_SUCCESS;
-    gpio_write_fake.return_val = W_SUCCESS;
-
-    // start
-    ads1219_start_fake.return_val = W_SUCCESS;
-
-    w_status_t status= adxrs649_init();
+    w_status_t status= adxl380_init();
     EXPECT_EQ(W_SUCCESS, status);
 };
 
-TEST_F(ADXRS649, initFailAfterADS1219InitFail){
+TEST_F(ADXL380, initFailWithADXL38XInitFail){
 
     // set up function returns
-    ads1219_init_fake.return_val = W_FAILURE;
+    adxl38x_init_fake.return_val = W_FAILURE;
 
     // set up
-    ads1219_set_channel_fake.return_val = W_SUCCESS;
-    ads1219_set_conversion_mode_fake.return_val = W_SUCCESS;
-    ads1219_set_gain_fake.return_val = W_SUCCESS;
-    ads1219_set_data_rate_fake.return_val = W_SUCCESS;
-    ads1219_set_vref_fake.return_val = W_SUCCESS;
+    adxl38x_soft_reset_fake.return_val = W_SUCCESS;
+    adxl38x_set_op_mode_fake.return_val = W_SUCCESS;
+    adxl38x_set_range_fake.return_val = W_SUCCESS;
+    adxl38x_register_update_bits_fake.return_val = W_SUCCESS;
 
-    // self-test
-    adxrs649_self_test_fake.return_val = W_SUCCESS;
-    gpio_write_fake.return_val = W_SUCCESS;
-
-    // start
-    ads1219_start_fake.return_val = W_SUCCESS;
-
-    w_status_t status= adxrs649_init();
+    w_status_t status= adxl380_init();
     EXPECT_EQ(W_FAILURE, status);
 };
 
-TEST_F(ADXRS649, initFailAfterSetUpFail){
+TEST_F(ADXL380, initFailWithADXL38XInitSettingFailure){
 
     // set up function returns
-    ads1219_init_fake.return_val = W_SUCCESS;
+    adxl38x_init_fake.return_val = W_SUCCESS;
 
     // set up
-    ads1219_set_channel_fake.return_val = W_FAILURE;
-    ads1219_set_conversion_mode_fake.return_val = W_SUCCESS;
-    ads1219_set_gain_fake.return_val = W_SUCCESS;
-    ads1219_set_data_rate_fake.return_val = W_SUCCESS;
-    ads1219_set_vref_fake.return_val = W_SUCCESS;
+    adxl38x_soft_reset_fake.return_val = W_SUCCESS;
+    adxl38x_set_op_mode_fake.return_val = W_FAILURE;
+    adxl38x_set_range_fake.return_val = W_SUCCESS;
+    adxl38x_register_update_bits_fake.return_val = W_SUCCESS;
 
-    // self-test
-    adxrs649_self_test_fake.return_val = W_SUCCESS;
-    gpio_write_fake.return_val = W_SUCCESS;
-
-    // start
-    ads1219_start_fake.return_val = W_SUCCESS;
-
-    w_status_t status= adxrs649_init();
-    EXPECT_NE(W_SUCCESS, status);
+    w_status_t status= adxl380_init();
+    EXPECT_EQ(W_FAILURE, status);
 };
 
-TEST_F(ADXRS649, initFailAfterSelfTestFail){
+TEST_F(ADXL380, initFailWithADXL38XSettingFail){
 
     // set up function returns
-    ads1219_init_fake.return_val = W_SUCCESS;
+    adxl38x_init_fake.return_val = W_SUCCESS;
 
     // set up
-    ads1219_set_channel_fake.return_val = W_SUCCESS;
-    ads1219_set_conversion_mode_fake.return_val = W_SUCCESS;
-    ads1219_set_gain_fake.return_val = W_SUCCESS;
-    ads1219_set_data_rate_fake.return_val = W_SUCCESS;
-    ads1219_set_vref_fake.return_val = W_SUCCESS;
+    adxl38x_soft_reset_fake.return_val = W_SUCCESS;
+    adxl38x_set_op_mode_fake.return_val = W_FAILURE;
+    adxl38x_set_range_fake.return_val = W_SUCCESS;
+    adxl38x_register_update_bits_fake.return_val = W_SUCCESS;
 
-    // self-test
-    adxrs649_self_test_fake.return_val = W_FAILURE;
-    gpio_write_fake.return_val = W_SUCCESS;
-
-    // start
-    ads1219_start_fake.return_val = W_SUCCESS;
-
-    w_status_t status= adxrs649_init();
+    w_status_t status= adxl380_init();
     EXPECT_EQ(W_FAILURE, status);
-    EXPECT_EQ(2, gpio_write_fake.call_count);
 };
 
-TEST_F(ADXRS649, initFailAfterSelfTestFailWithGPIO){
+TEST_F(ADXL380, initFailWithADXL38XUpdateBitFail){
 
     // set up function returns
-    ads1219_init_fake.return_val = W_SUCCESS;
+    adxl38x_init_fake.return_val = W_SUCCESS;
 
     // set up
-    ads1219_set_channel_fake.return_val = W_SUCCESS;
-    ads1219_set_conversion_mode_fake.return_val = W_SUCCESS;
-    ads1219_set_gain_fake.return_val = W_SUCCESS;
-    ads1219_set_data_rate_fake.return_val = W_SUCCESS;
-    ads1219_set_vref_fake.return_val = W_SUCCESS;
+    adxl38x_soft_reset_fake.return_val = W_SUCCESS;
+    adxl38x_set_op_mode_fake.return_val = W_SUCCESS;
+    adxl38x_set_range_fake.return_val = W_SUCCESS;
+    adxl38x_register_update_bits_fake.return_val = W_FAILURE;
 
-    // self-test
-    adxrs649_self_test_fake.return_val = W_FAILURE;
-    gpio_write_fake.return_val = W_FAILURE;
-
-    // start
-    ads1219_start_fake.return_val = W_SUCCESS;
-
-    w_status_t status= adxrs649_init();
+    w_status_t status= adxl380_init();
     EXPECT_EQ(W_FAILURE, status);
-    EXPECT_EQ(2, gpio_write_fake.call_count);
 };
 
-
-TEST_F(ADXRS649, initFailAfterStartFail){
+TEST_F(ADXL380, initFailWithADXL38XUpdateBitFail){
 
     // set up function returns
-    ads1219_init_fake.return_val = W_SUCCESS;
+    adxl38x_init_fake.return_val = W_SUCCESS;
 
     // set up
-    ads1219_set_channel_fake.return_val = W_SUCCESS;
-    ads1219_set_conversion_mode_fake.return_val = W_SUCCESS;
-    ads1219_set_gain_fake.return_val = W_SUCCESS;
-    ads1219_set_data_rate_fake.return_val = W_SUCCESS;
-    ads1219_set_vref_fake.return_val = W_SUCCESS;
+    adxl38x_soft_reset_fake.return_val = W_SUCCESS;
+    adxl38x_set_op_mode_fake.return_val = W_SUCCESS;
+    adxl38x_set_range_fake.return_val = W_SUCCESS;
+    adxl38x_register_update_bits_fake.return_val = W_FAILURE;
 
-    // self-test
-    adxrs649_self_test_fake.return_val = W_SUCCESS;
-    gpio_write_fake.return_val = W_SUCCESS;
-
-    // start
-    ads1219_start_fake.return_val = W_FAILURE;
-
-    w_status_t status= adxrs649_init();
+    w_status_t status= adxl380_init();
     EXPECT_EQ(W_FAILURE, status);
 };
 
-TEST_F(ADXRS649, getGyroDataFailByBothDRDYFail){
+TEST_F(ADXL380, initFailWithADXL38XUpdateBitFail){
 
-    // read DRDY
-    gpio_read_fake.return_val = W_FAILURE;
-    ads1219_conversion_ready_fake.return_val = W_FAILURE;
+    // set up function returns
+    adxl38x_init_fake.return_val = W_SUCCESS;
 
-    // read value
-    ads1219_read_value_fake.return_val = W_SUCCESS;
-    ads1219_millivolts_fake.return_val = W_SUCCESS;
+    // set up
+    adxl38x_soft_reset_fake.return_val = W_SUCCESS;
+    adxl38x_set_op_mode_fake.return_val = W_SUCCESS;
+    adxl38x_set_range_fake.return_val = W_SUCCESS;
+    adxl38x_register_update_bits_fake.return_val = W_FAILURE;
 
-    float data = 0;
-    uint32_t raw_data = 0;
-    w_status_t status= adxrs649_get_gyro_data(&data, &raw_data);
-    EXPECT_EQ(W_IO_ERROR, status);
-};
-
-TEST_F(ADXRS649, getGyroDataFailByGPIORead0){
-
-    // read DRDY
-    gpio_set_read_fake.custome_fake = gpio_set_read;
-    global_gpio_value = GPIO_LEVEL_HIGH;
-    gpio_return_value = W_SUCCESS; 
-
-    ads1219_conversion_ready_fake.return_val = W_SUCCESS;
-
-    // read value
-    ads1219_read_value_fake.return_val = W_SUCCESS;
-    ads1219_millivolts_fake.return_val = W_SUCCESS;
-
-    float data = 0;
-    uint32_t raw_data = 0;
-    w_status_t status= adxrs649_get_gyro_data(&data, &raw_data);
+    w_status_t status= adxl380_init();
     EXPECT_EQ(W_FAILURE, status);
 };
 
-TEST_F(ADXRS649, getGyroDataFailWithGPIOFailADS1219SuceessNotReady){
+TEST_F(ADXL380, getRawAccelFail){
 
-    // read DRDY
-    gpio_read_fake.return_val = W_SUCCESS;
+    // set up function returns
+    adxl38x_read_device_data_fake.return_val = W_FAILURE;
 
-    global_ads1219_conversion_ready = false;
-    ads1219_conversion_ready_return = W_SUCCESS;
-    ads1219_conversion_ready_fake.custom_fake = ads1219_set_conversion_ready;
+    altimu_raw_imu_data_t raw_data = {0};
 
-    // read value
-    ads1219_read_value_fake.return_val = W_SUCCESS;
-
-    ads1219_millivolts_fake.return_val = W_SUCCESS;
-
-    float data = 0;
-    uint32_t raw_data = 0;
-    w_status_t status= adxrs649_get_gyro_data(&data, &raw_data);
+    w_status_t status= adxl380_get_raw_accel(&raw_data);
     EXPECT_EQ(W_FAILURE, status);
 };
 
-TEST_F(ADXRS649, getGyroDataFailByReadValFail){
+TEST_F(ADXL380, getRawAccelSuccess){
 
-    // read DRDY
-    gpio_read_fake.return_val = W_SUCCESS;
-    ads1219_conversion_ready_fake.return_val = W_SUCCESS;
+    // set up function returns
+    adxl38x_read_device_data_fake.return_val = W_FAILURE;
 
-    // read value
-    ads1219_read_value_fake.return_val = W_FAILURE;
-    ads1219_millivolts_fake.return_val = W_SUCCESS;
+    altimu_raw_imu_data_t raw_data = {0};
 
-    float data = 0;
-    uint32_t raw_data = 0;
-    w_status_t status= adxrs649_get_gyro_data(&data, &raw_data);
-    EXPECT_EQ(W_IO_ERROR, status);
-};
-
-TEST_F(ADXRS649, getGyroDataFailByConvFail){
-
-    // read DRDY
-    gpio_read_fake.return_val = W_SUCCESS;
-    ads1219_conversion_ready_fake.return_val = W_SUCCESS;
-
-    // read value
-    ads1219_read_value_fake.return_val = W_SUCCESS;
-    ads1219_millivolts_fake.return_val = W_FAILURE;
-
-    float data = 0;
-    uint32_t raw_data = 0;
-    w_status_t status= adxrs649_get_gyro_data(&data, &raw_data);
+    w_status_t status= adxl380_get_raw_accel(&raw_data);
     EXPECT_EQ(W_FAILURE, status);
-};
-
-TEST_F(ADXRS649, getGyroDataSuccessWithoutErrorMax){
-
-    // read DRDY
-    gpio_read_fake.return_val = W_SUCCESS;
-    ads1219_conversion_ready_fake.return_val = W_SUCCESS;
-
-    // read value
-    ads1219_read_value_fake.return_val = W_SUCCESS;
-
-    global_adc_output_mv = 4500;
-    ads1219_set_millivolts_return = W_SUCCESS;
-    ads1219_millivolts_fake.custom_fake = ads1219_set_millivolts;
-
-    float data = 0;
-    uint32_t raw_data = 0;
-    w_status_t status= adxrs649_get_gyro_data(&data, &raw_data);
-    EXPECT_EQ(W_SUCCESS, status);
-    EXPECT_EQ(((global_adc_output_mv - 2500) / 0.1), data);
 };
