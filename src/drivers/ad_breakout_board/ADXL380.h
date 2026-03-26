0509031c05814ab2d224bc11203bb5c0d6aeacbb
Shiming He <eshiminghe@gmail.com>
2026-03-26 19:13:46 -0400

    match barr varaible naming standard and add new test for adxl380 init

---
+++ src/drivers/ad_breakout_board/ADXL380.h
@@ -0,0 +1,30 @@
+#ifndef ADXL380_H
+#define ADXL380_H
+
+#include "drivers/altimu-10/altimu-10.h"
+#include "rocketlib/include/common.h"
+#include <stdbool.h>
+#include <stdint.h>
+
+/**
+ * @brief this is initializes the ADXL380
+ * @return the status of the function call
+ */
+w_status_t adxl380_init();
+
+/**
+ * @brief this gets the raw acceleration data from the ADXL380
+ * @param p_raw_data pointer to all of the raw data for each access
+ * @return the status of the function call
+ */
+w_status_t adxl380_get_raw_accel(altimu_raw_imu_data_t *p_raw_data);
+
+/**
+ * @brief this gets the acceleration data (raw and processed) from the ADXL380
+ * @param data pointer to the vector form of the acceleration
+ * @param p_raw_data pointer to all of the raw data for each access
+ * @return the status of the function call
+ */
+w_status_t adxl380_get_accel_data(vector3d_t *data, altimu_raw_imu_data_t *p_raw_data);
+
+#endif
--- tests/unit/ad_breakout_board_test.cpp
+++ tests/unit/ad_breakout_board_test.cpp
@@ -6,6 +6,7 @@
 extern "C" {
     #include "drivers/ad_breakout_board/ad_breakout_board.h"
     #include "rocketlib/include/common.h"
+    #include "drivers/ad_breakout_board/ADXL380.h"
     #include "drivers/ad_breakout_board/ADXRS649.h"
     #include "application/logger/log.h"
     #include "drivers/timer/timer.h"
@@ -13,7 +14,9 @@ extern "C" {
     extern w_status_t ad_beakout_board_init();
 
     FAKE_VALUE_FUNC(w_status_t, adxrs649_init);
+    FAKE_VALUE_FUNC(w_status_t, adxl380_init);
     FAKE_VALUE_FUNC(w_status_t, adxrs649_get_gyro_data, float*, int32_t*);
+    FAKE_VALUE_FUNC(w_status_t, adxl380_get_accel_data, vector3d_t *, altimu_raw_imu_data_t *);
     FAKE_VALUE_FUNC(w_status_t, timer_get_ms, float*);
     FAKE_VALUE_FUNC_VARARG(w_status_t, log_text, uint32_t, const char *, const char *, ...);
 
@@ -23,12 +26,17 @@ class ADBreakoutBoardTets : public ::testing::Test {
 protected:
     void SetUp() override {
         RESET_FAKE(adxrs649_init);
+        RESET_FAKE(adxl380_init);
+        RESET_FAKE(adxrs649_get_gyro_data);
+        RESET_FAKE(adxl380_get_accel_data);
+        RESET_FAKE(timer_get_ms);
         RESET_FAKE(log_text);
     };
 };
 
 TEST_F(ADBreakoutBoardTets, InitSuccess) {
     adxrs649_init_fake.return_val = W_SUCCESS;
+    adxl380_init_fake.return_val = W_SUCCESS;
 
     w_status_t status = ad_beakout_board_init();
 
@@ -36,9 +44,19 @@ TEST_F(ADBreakoutBoardTets, InitSuccess) {
     EXPECT_EQ(status, W_SUCCESS);
 }
 
-
 TEST_F(ADBreakoutBoardTets, InitFailureADXRS) {
     adxrs649_init_fake.return_val = W_FAILURE;
+    adxl380_init_fake.return_val = W_SUCCESS;
+
+    w_status_t status = ad_beakout_board_init();
+
+    EXPECT_EQ(adxrs649_init_fake.call_count, 1);
+    EXPECT_EQ(status, W_FAILURE);
+}
+
+TEST_F(ADBreakoutBoardTets, InitFailureADXL) {
+    adxrs649_init_fake.return_val = W_SUCCESS;
+    adxl380_init_fake.return_val = W_FAILURE;
 
     w_status_t status = ad_beakout_board_init();
 
