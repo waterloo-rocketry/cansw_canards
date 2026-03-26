ddf24f7623e85b5ada827ff845af4402bc560bd7
Shiming He <eshiminghe@gmail.com>
2026-03-26 16:14:55 -0400

    fix i2c2 fall time

--- src/third_party/cubemx_autogen/src/i2c.c
+++ src/third_party/cubemx_autogen/src/i2c.c
@@ -89,7 +89,7 @@ void MX_I2C2_Init(void)
 
   /* USER CODE END I2C2_Init 1 */
   hi2c2.Instance = I2C2;
-  hi2c2.Init.Timing = 0x00601A5A;
+  hi2c2.Init.Timing = 0x00601A57;
   hi2c2.Init.OwnAddress1 = 0;
   hi2c2.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
   hi2c2.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
