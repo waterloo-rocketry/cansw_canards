50c9f1df6063d680528fc1ad3d14a99b994c6b3c
Shiming He <eshiminghe@gmail.com>
2026-03-26 18:34:36 -0400

    change NO-OS driver to match BARR naming stanadard

--- src/drivers/ad_breakout_board/adxl38x.h
+++ src/drivers/ad_breakout_board/adxl38x.h
@@ -24,52 +24,54 @@ typedef struct {
 uint8_t adxl38x_field_prep_u8(uint8_t mask, uint8_t value);
 
 // MAIN DRIVER
-w_status_t adxl38x_read_device_data(adxl38x_dev_t *dev, uint8_t base_address, uint16_t size,
-									uint8_t *read_data);
+w_status_t adxl38x_read_device_data(adxl38x_dev_t *p_dev, uint8_t base_address, uint16_t size,
+									uint8_t *p_read_data);
 
-w_status_t adxl38x_write_device_data(adxl38x_dev_t *dev, uint8_t base_address, uint16_t size,
-									 uint8_t *write_data);
+w_status_t adxl38x_write_device_data(adxl38x_dev_t *p_dev, uint8_t base_address, uint16_t size,
+									 uint8_t *p_write_data);
 
-w_status_t adxl38x_register_update_bits(adxl38x_dev_t *dev, uint8_t reg_addr, uint8_t mask,
+w_status_t adxl38x_register_update_bits(adxl38x_dev_t *p_dev, uint8_t reg_addr, uint8_t mask,
 										uint8_t update_val);
 
-w_status_t adxl38x_init(adxl38x_dev_t *dev);
+w_status_t adxl38x_init(adxl38x_dev_t *p_dev);
 
-w_status_t adxl38x_soft_reset(adxl38x_dev_t *dev);
+w_status_t adxl38x_soft_reset(adxl38x_dev_t *p_dev);
 
-w_status_t adxl38x_set_op_mode(adxl38x_dev_t *dev, adxl38x_op_mode_t op_mode);
+w_status_t adxl38x_set_op_mode(adxl38x_dev_t *p_dev, adxl38x_op_mode_t op_mode);
 
-w_status_t adxl38x_get_op_mode(adxl38x_dev_t *dev, adxl38x_op_mode_t *op_mode);
+w_status_t adxl38x_get_op_mode(adxl38x_dev_t *p_dev, adxl38x_op_mode_t *p_op_mode);
 
-w_status_t adxl38x_set_range(adxl38x_dev_t *dev, adxl38x_range_t range_val);
+w_status_t adxl38x_set_range(adxl38x_dev_t *p_dev, adxl38x_range_t range_val);
 
-w_status_t adxl38x_get_range(adxl38x_dev_t *dev, adxl38x_range_t *range_val);
+w_status_t adxl38x_get_range(adxl38x_dev_t *p_dev, adxl38x_range_t *p_range_val);
 
-w_status_t adxl38x_set_self_test_registers(adxl38x_dev_t *dev, bool st_mode, bool st_force,
+w_status_t adxl38x_set_self_test_registers(adxl38x_dev_t *p_dev, bool st_mode, bool st_force,
 										   bool st_dir);
 
-w_status_t adxl38x_clear_self_test_registers(adxl38x_dev_t *dev);
+w_status_t adxl38x_clear_self_test_registers(adxl38x_dev_t *p_dev);
 
-w_status_t adxl38x_get_sts_reg(adxl38x_dev_t *dev, adxl38x_sts_reg_flags_t *status_flags);
+w_status_t adxl38x_get_sts_reg(adxl38x_dev_t *p_dev, adxl38x_sts_reg_flags_t *p_status_flags);
 
-w_status_t adxl38x_get_raw_xyz(adxl38x_dev_t *dev, uint16_t *raw_x, uint16_t *raw_y,
-							   uint16_t *raw_z);
+w_status_t adxl38x_get_raw_xyz(adxl38x_dev_t *p_dev, uint16_t *p_raw_x, uint16_t *p_raw_y,
+							   uint16_t *p_raw_z);
 
-w_status_t adxl38x_get_temp(adxl38x_dev_t *dev, float *temp_degC);
+w_status_t adxl38x_get_temp(adxl38x_dev_t *p_dev, float *p_temp_degC);
 
-w_status_t adxl38x_get_raw_data(adxl38x_dev_t *dev, adxl38x_ch_select_t channels, uint16_t *raw_x,
-								uint16_t *raw_y, uint16_t *raw_z, uint16_t *raw_temp);
+w_status_t adxl38x_get_raw_data(adxl38x_dev_t *p_dev, adxl38x_ch_select_t channels,
+								uint16_t *p_raw_x, uint16_t *p_raw_y, uint16_t *p_raw_z,
+								uint16_t *p_raw_temp);
 
-w_status_t adxl38x_get_xyz_gees(adxl38x_dev_t *dev, adxl38x_ch_select_t channels, float *x,
-								float *y, float *z);
+w_status_t adxl38x_get_xyz_gees(adxl38x_dev_t *p_dev, adxl38x_ch_select_t channels, float *p_x,
+								float *p_y, float *p_z);
 
-w_status_t adxl38x_selftest(adxl38x_dev_t *dev, adxl38x_op_mode_t op_mode, bool *st_x, bool *st_y,
-							bool *st_z);
+w_status_t adxl38x_selftest(adxl38x_dev_t *p_dev, adxl38x_op_mode_t op_mode, bool *p_st_x,
+							bool *p_st_y, bool *p_st_z);
 
-w_status_t adxl38x_accel_set_FIFO(adxl38x_dev_t *dev, uint16_t num_samples, bool external_trigger,
+w_status_t adxl38x_accel_set_FIFO(adxl38x_dev_t *p_dev, uint16_t num_samples, bool external_trigger,
 								  adxl38x_fifo_mode_t fifo_mode, bool ch_ID_enable,
 								  bool read_reset);
 
-w_status_t adxl38x_data_raw_to_gees(adxl38x_dev_t *dev, uint8_t *raw_accel_data, float *data_frac);
+w_status_t adxl38x_data_raw_to_gees(adxl38x_dev_t *p_dev, uint8_t *p_raw_accel_data,
+									float *p_data_frac);
 
 #endif // ADXL38X_H
