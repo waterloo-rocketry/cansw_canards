e67e50e5363930acf26e75e96167f29edc2f0c87
Shiming He <eshiminghe@gmail.com>
2026-03-26 19:44:11 +0000

    add unit test

--- tests/unit/ads1219_test.cpp
+++ tests/unit/ads1219_test.cpp
@@ -18,7 +18,7 @@ uint32_t i2c_test_input;
 ads1219_handle_t test_handle = {.bus = I2C_BUS_4, .i2c_addr = 0x40};
 w_status_t i2c_read_reg_custom_output(i2c_bus_t bus, uint8_t device_addr, uint8_t reg, uint8_t *data, uint8_t len){
 
-    uint8_t test_num_array[3] = {(uint8_t) ((((uint32_t) i2c_test_input) & 0xFF0000) >> 16), (uint8_t) ((((uint32_t) i2c_test_input) & 0xFF00) >> 8), (uint8_t) ((uint32_t) i2c_test_input) & 0xFF};
+    uint8_t test_num_array[3] = {(uint8_t) ((((uint32_t) i2c_test_input) & 0xFF0000) >> 16), (uint8_t) ((((uint32_t) i2c_test_input) & 0xFF00) >> 8), static_cast<uint8_t>((uint32_t) i2c_test_input & 0xFF)};
     uint8_t* i2c_custom_output = (uint8_t*) test_num_array;
     data[0] = i2c_custom_output[0];
     data[1] = i2c_custom_output[1];
