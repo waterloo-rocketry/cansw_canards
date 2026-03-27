059cd6ff3bad43bd5650162856808c57f9be2cf2
Shiming He <eshiminghe@gmail.com>
2026-03-27 03:21:30 +0000

    add rate limiter and associated unit test

---
+++ src/common/rate_limiter/rate_limiter.h
@@ -0,0 +1,21 @@
+#ifndef RATE_LIMITER_H
+#define RATE_LIMITER_H
+#include <stdbool.h>
+#include <stdint.h>
+
+/**
+ * @brief provides control to rate limit any call
+ * @param freq the frequency that is being rate limiting to
+ * @param current_ms the current time
+ * @param p_last_ms pointer to the last operated time (this will be updated automatically)
+ * @return if it's time to run the call
+ */
+static inline bool rate_limiter(const uint16_t freq, double current_ms, double * p_last_ms) {
+    if ((current_ms - (*p_last_ms)) >= (1000 / freq)) {
+        *p_last_ms = current_ms;
+        return true;
+    }
+    return false;
+}
+
+#endif
