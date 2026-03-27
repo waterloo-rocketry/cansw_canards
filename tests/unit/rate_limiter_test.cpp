69290b5259c04f308d1c2c67bb18776cd07d8f93
Shiming He <eshiminghe@gmail.com>
2026-03-27 01:20:06 -0400

    change rate limiter to use uint32_t

--- tests/unit/rate_limiter_test.cpp
+++ tests/unit/rate_limiter_test.cpp
@@ -16,61 +16,55 @@ protected:
 };
 
 TEST_F(RateLimiterTest, RateLimiterSuccess1) {
-    double last_time = 177457957100.2;
-    EXPECT_EQ(true, rate_limiter(100, 177457957120.7, &last_time));
-    EXPECT_FLOAT_EQ(177457957120.7, last_time);
+    uint32_t last_time = 13012100;
+    EXPECT_EQ(true, rate_limiter(100, 13012120, &last_time));
+    EXPECT_EQ(13012120, last_time);
 }
 
 TEST_F(RateLimiterTest, RateLimiterSuccessOnLimit) {
-    double last_time = 177457957100;
-    EXPECT_EQ(true, rate_limiter(4, 177457957350, &last_time));
-    EXPECT_FLOAT_EQ(177457957350, last_time);
+    uint32_t last_time = 13012100;
+    EXPECT_EQ(true, rate_limiter(4, 13012350, &last_time));
+    EXPECT_EQ(13012350, last_time);
 }
 
 TEST_F(RateLimiterTest, RateLimiterFailureWithSameTime) {
-    double last_time = 177457957100.5;
-    EXPECT_EQ(false, rate_limiter(4, 177457957100.5, &last_time));
-    EXPECT_FLOAT_EQ(177457957100.5, last_time);
+    uint32_t last_time = 13012100;
+    EXPECT_EQ(false, rate_limiter(4, 13012100, &last_time));
+    EXPECT_EQ(13012100, last_time);
 }
 
 TEST_F(RateLimiterTest, RateLimiterFailureRegular) {
-    double last_time = 177457957100.2;
-    EXPECT_EQ(false, rate_limiter(4, 177457957300, &last_time));
-    EXPECT_FLOAT_EQ(177457957100.2, last_time);
+    uint32_t last_time = 13012100;
+    EXPECT_EQ(false, rate_limiter(4, 13012300, &last_time));
+    EXPECT_EQ(13012100, last_time);
 }
 
 TEST_F(RateLimiterTest, RateLimiterFailureBelowBorder) {
-    double last_time = 177457957100.2;
-    EXPECT_EQ(false, rate_limiter(4, 177457957350.1, &last_time));
-    EXPECT_FLOAT_EQ(177457957100.2, last_time);
+    uint32_t last_time = 13012100;
+    EXPECT_EQ(false, rate_limiter(4, 13012349, &last_time));
+    EXPECT_EQ(13012100, last_time);
 }
 
 TEST_F(RateLimiterTest, RateLimiterSuccessStartFrom0) {
-    double last_time = 0;
-    EXPECT_EQ(true, rate_limiter(250, 4.2, &last_time));
-    EXPECT_FLOAT_EQ(4.2, last_time);
+    uint32_t last_time = 0;
+    EXPECT_EQ(true, rate_limiter(250, 5, &last_time));
+    EXPECT_EQ(5, last_time);
 }
 
 TEST_F(RateLimiterTest, RateLimiterFailureLastTimeLarger) {
-    double last_time = 230;
-    EXPECT_EQ(false, rate_limiter(4, 100.2, &last_time));
-    EXPECT_FLOAT_EQ(230, last_time);
+    uint32_t last_time = 230;
+    EXPECT_EQ(false, rate_limiter(4, 100, &last_time));
+    EXPECT_EQ(230, last_time);
 }
 
-TEST_F(RateLimiterTest, RateLimiterFailureNeg1) {
-    double last_time = 230;
-    EXPECT_EQ(false, rate_limiter(4, -100.2, &last_time));
-    EXPECT_FLOAT_EQ(230, last_time);
+TEST_F(RateLimiterTest, RateLimiterSuccessAtMax) {
+    uint32_t last_time = UINT32_MAX - 2;
+    EXPECT_EQ(true, rate_limiter(500, UINT32_MAX, &last_time));
+    EXPECT_EQ(UINT32_MAX, last_time);
 }
 
-TEST_F(RateLimiterTest, RateLimiterFailureNeg2) {
-    double last_time = -230;
-    EXPECT_EQ(false, rate_limiter(4, 100.2, &last_time));
-    EXPECT_FLOAT_EQ(-230, last_time);
-}
-
-TEST_F(RateLimiterTest, RateLimiterFailureNeg3) {
-    double last_time = -230;
-    EXPECT_EQ(false, rate_limiter(1000, -228.5, &last_time));
-    EXPECT_FLOAT_EQ(-230, last_time);
+TEST_F(RateLimiterTest, RateLimiterFailurePassMax) {
+    uint32_t last_time = UINT32_MAX;
+    EXPECT_EQ(false, rate_limiter(500, (uint32_t) (((uint64_t) UINT32_MAX) + 2), &last_time));
+    EXPECT_EQ(UINT32_MAX, last_time);
 }
