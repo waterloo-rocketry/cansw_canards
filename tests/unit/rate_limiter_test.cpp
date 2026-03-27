81ea5e25d7752a20949b16f338d242c4b0809861
Shiming He <eshiminghe@gmail.com>
2026-03-27 03:21:30 +0000

    add rate limiter and associated unit test

---
+++ tests/unit/rate_limiter_test.cpp
@@ -0,0 +1,58 @@
+#include "fff.h"
+#include <gtest/gtest.h>
+
+extern "C" {
+// add includes like freertos, hal, proc headers, etc
+#include "common/rate_limiter/rate_limiter.h"
+#include <stdbool.h>
+#include <stdint.h>
+}
+
+class RateLimiterTest : public ::testing::Test {
+protected:
+    void SetUp() override {}
+
+    void TearDown() override {}
+};
+
+TEST_F(RateLimiterTest, RateLimiterSuccess1) {
+    double last_time = 177457957100.2;
+    EXPECT_EQ(true, rate_limiter(100, 177457957120.7, &last_time));
+    EXPECT_FLOAT_EQ(177457957120.7, last_time);
+}
+
+TEST_F(RateLimiterTest, RateLimiterSuccessOnLimit) {
+    double last_time = 177457957100;
+    EXPECT_EQ(true, rate_limiter(4, 177457957350, &last_time));
+    EXPECT_FLOAT_EQ(177457957350, last_time);
+}
+
+TEST_F(RateLimiterTest, RateLimiterFailureWithSameTime) {
+    double last_time = 177457957100.5;
+    EXPECT_EQ(false, rate_limiter(4, 177457957100.5, &last_time));
+    EXPECT_FLOAT_EQ(177457957100.5, last_time);
+}
+
+TEST_F(RateLimiterTest, RateLimiterFailureRegular) {
+    double last_time = 177457957100.2;
+    EXPECT_EQ(false, rate_limiter(4, 177457957300, &last_time));
+    EXPECT_FLOAT_EQ(177457957100.2, last_time);
+}
+
+TEST_F(RateLimiterTest, RateLimiterFailureBelowBorder) {
+    double last_time = 177457957100.2;
+    EXPECT_EQ(false, rate_limiter(4, 177457957350.1, &last_time));
+    EXPECT_FLOAT_EQ(177457957100.2, last_time);
+}
+
+TEST_F(RateLimiterTest, RateLimiterSuccessStartFrom0) {
+    double last_time = 0;
+    EXPECT_EQ(true, rate_limiter(250, 4.2, &last_time));
+    EXPECT_FLOAT_EQ(4.2, last_time);
+}
+
+TEST_F(RateLimiterTest, RateLimiterFailureLastTimeLarger) {
+    double last_time = 230;
+    EXPECT_EQ(false, rate_limiter(4, 100.2, &last_time));
+    EXPECT_FLOAT_EQ(230, last_time);
+}
