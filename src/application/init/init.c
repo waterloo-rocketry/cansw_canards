40799e7f6f98e51b7da93962f4beb761d91b2a65
Shiming He <eshiminghe@gmail.com>
2026-04-24 17:38:20 -0400

    test code

--- src/application/init/init.c
+++ src/application/init/init.c
@@ -23,6 +23,7 @@
 #include "i2c.h" // For hi2c2, hi2c4
 #include "task.h"
 #include "usart.h"
+#include "sdmmc.h"
 
 // Maximum number of initialization retries before giving up
 #define MAX_INIT_RETRIES 1
@@ -163,6 +164,36 @@ static void system_init_task(void *arg) {
 		proc_handle_fatal_error("tasks");
 	}
 	log_text(10, "SystemInit", "All tasks created successfully.");
+
+	// test SD card time !!!! :(
+	// HAL_StatusTypeDef sd_status  = HAL_SD_Init(&hsd2); 
+	uint32_t err = HAL_SD_GetError(&hsd2);
+	HAL_SD_StateTypeDef sd_state = HAL_SD_GetState(&hsd2);
+	HAL_SD_CardStateTypeDef sdcard_state = HAL_SD_GetCardState(&hsd2);
+
+	if (err != 0 && sd_state != 0 && sdcard_state != 0) {
+		vTaskDelay(500);
+	}
+  	HAL_SD_CardInfoTypeDef info;
+	HAL_StatusTypeDef sd_status  = HAL_SD_GetCardInfo(&hsd2, &info);
+	err = HAL_SD_GetError(&hsd2);
+	sd_state = HAL_SD_GetState(&hsd2);
+	sdcard_state = HAL_SD_GetCardState(&hsd2);
+	if (HAL_OK != sd_status && err != 0 && sd_state != 0 && sdcard_state != 0) {
+		vTaskDelay(500);
+	}
+
+
+  	uint8_t rx[512];
+	uint32_t test_lba = 1024;
+	sd_status = HAL_SD_ReadBlocks(&hsd2, rx, test_lba, 1, 1000);
+  	vTaskDelay(1000);
+	sdcard_state = HAL_SD_GetCardState(&hsd2);
+	if (HAL_OK != sd_status && err != 0 && sd_state != 0 && sdcard_state != 0) {
+		vTaskDelay(500);
+	}
+
+
 
 	// its blinky now
 	while (1) {
