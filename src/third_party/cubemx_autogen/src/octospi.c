22968a115b669eb9df17988ddf19ec82b0cd9f65
Shiming He <eshiminghe@gmail.com>
2026-04-27 16:05:43 -0400

    update chip select high time

--- src/third_party/cubemx_autogen/src/octospi.c
+++ src/third_party/cubemx_autogen/src/octospi.c
@@ -44,7 +44,7 @@ void MX_OCTOSPI1_Init(void)
   hospi1.Init.DualQuad = HAL_OSPI_DUALQUAD_DISABLE;
   hospi1.Init.MemoryType = HAL_OSPI_MEMTYPE_MICRON;
   hospi1.Init.DeviceSize = 32;
-  hospi1.Init.ChipSelectHighTime = 1;
+  hospi1.Init.ChipSelectHighTime = 3;
   hospi1.Init.FreeRunningClock = HAL_OSPI_FREERUNCLK_DISABLE;
   hospi1.Init.ClockMode = HAL_OSPI_CLOCK_MODE_0;
   hospi1.Init.WrapSize = HAL_OSPI_WRAP_NOT_SUPPORTED;
