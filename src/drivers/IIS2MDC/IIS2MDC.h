b635e9041b2d322df10b020aed4d716c2a9528ec
Michael Yu <zk.yu26@gmail.com>
2026-05-18 19:42:47 -0400

    format.

--- src/drivers/IIS2MDC/IIS2MDC.h
+++ src/drivers/IIS2MDC/IIS2MDC.h
@@ -13,14 +13,14 @@
 // Expected WHO_AM_I value for sanity checking
 #define IIS2MDC_WHO_AM_I_VAL 0x40
 
-//Register addresses
-#define IIS2MDC_REG_OFFSET_X_L 0x45 //add other axes later
+// Register addresses
+#define IIS2MDC_REG_OFFSET_X_L 0x45 // add other axes later
 #define IIS2MDC_REG_WHO_AM_I 0x4F
 #define IIS2MDC_REG_CFG_A 0x60
 #define IIS2MDC_REG_CFG_B 0x61
 #define IIS2MDC_REG_CFG_C 0x62
 #define IIS2MDC_REG_STATUS 0x67
-#define IIS2MDC_REG_OUTX_L 0x68 //add other axes later
+#define IIS2MDC_REG_OUTX_L 0x68 // add other axes later
 
 // Status register for when a new set of data available
 #define IIS2MDC_STATUS_ZYXDA (1 << 3)
