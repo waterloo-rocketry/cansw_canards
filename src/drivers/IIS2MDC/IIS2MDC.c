69a295a82376a9fbba51338df5e2f390ac1dedb1
Michael Yu <zk.yu26@gmail.com>
2026-05-23 00:55:02 -0400

    function stubs for coding style check

--- src/drivers/IIS2MDC/IIS2MDC.c
+++ src/drivers/IIS2MDC/IIS2MDC.c
@@ -9,3 +9,13 @@
 #define IIS2MDC_REG_CFG_C 0x62
 #define IIS2MDC_REG_STATUS 0x67
 #define IIS2MDC_REG_OUTX_L 0x68
+
+w_status_t iis2mdc_init(void){
+	return W_SUCCESS;
+}
+w_status_t iis2mdc_check_sanity(void){
+	return W_SUCCESS;
+}
+w_status_t iis2mdc_get_data(iis2mdc_data_t *data, iis2mdc_raw_data_t *raw_data){
+	return W_SUCCESS;
+}
