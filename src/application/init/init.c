a149a1b0148c94e39f13a1eeb117998f736d35b9
Shiming He <eshiminghe@gmail.com>
2026-05-25 22:16:40 -0400

    update init and 54 address

--- src/application/init/init.c
+++ src/application/init/init.c
@@ -171,17 +171,24 @@ static void system_init_task(void *arg) {
 	if (W_SUCCESS != status) {
 		vTaskDelay(500);
 	}
+	uint16_t i = 0;
 	while (1) {
-		gpio_toggle(GPIO_PIN_RED_LED, 1);
-		gpio_toggle(GPIO_PIN_GREEN_LED, 1);
-		gpio_toggle(GPIO_PIN_BLUE_LED, 1);
+		if (i == 100) {
+			gpio_toggle(GPIO_PIN_RED_LED, 1);
+			gpio_toggle(GPIO_PIN_GREEN_LED, 1);
+			gpio_toggle(GPIO_PIN_BLUE_LED, 1);
+			i =0;
+		} else {
+			i++;
+		}
+		
 
 		status = adxl380_get_accel_data(&data, &raw_data);
 		
 		if (W_SUCCESS != status) {
 			vTaskDelay(500);
 		}
-		vTaskDelay(500);
+		vTaskDelay(5);
 			
 		log_text(10, "SystemInit", "ADXL380 DATA: corrected: x %lf y %lf z %lf raw: x %d y %d z %d", data.x, data.y, data.z, raw_data.x, raw_data.y, raw_data.z);
 	}
