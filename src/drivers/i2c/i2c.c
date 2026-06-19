1a4b32f82f0a3f7a75be9573e7034093b4a2a70a
Shiman <167009963+Shiman-Zhu@users.noreply.github.com>
2026-06-18 20:41:07 -0400

    is this reverting now?

--- src/drivers/i2c/i2c.c
+++ src/drivers/i2c/i2c.c
@@ -329,9 +329,7 @@ void i2c_reset_all(void) {
 	}
 }
 
-uint32_t i2c_get_status(void) {
-	uint32_t status_bitfield = 0;
-
+health_status_t i2c_get_status(void) {
 	// Check all i2c init
 	uint32_t num_bus_init = 0;
 	for (int i = 0; i < I2C_BUS_COUNT; i++) {
@@ -373,6 +371,8 @@ uint32_t i2c_get_status(void) {
 				 i2c_error_stats[i].nacks,
 				 i2c_error_stats[i].bus_errors);
 	}
+	
+	health_status_t status = {.severity = HEALTH_OK, .module_id = MODULE_I2C, .error_bitfield = 0};
 
-	return status_bitfield;
+	return status;
 }
