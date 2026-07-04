78ee08da18e1ab937cabbbdfd6f23c6103f45024
Maxim Beauchemin <maxim.beauchemin07@gmail.com>
2026-07-03 22:28:55 -0400

    format

--- src/drivers/lsm6dsv32x/LSM6DSV32X.h
+++ src/drivers/lsm6dsv32x/LSM6DSV32X.h
@@ -5,8 +5,8 @@
 
 #include "rocketlib/include/common.h"
 
-#include "common/math/math.h"
 #include "application/health_checks/health_checks.h"
+#include "common/math/math.h"
 
 /**
  * raw data from imu/mag registers
