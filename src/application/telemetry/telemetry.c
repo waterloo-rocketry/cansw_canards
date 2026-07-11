02042453200f36813a40bcc0b4d812b296b9c68d
Shiman <167009963+Shiman-Zhu@users.noreply.github.com>
2026-07-11 18:31:12 -0400

    oops, more fixed to pass ci

--- src/application/telemetry/telemetry.c
+++ src/application/telemetry/telemetry.c
@@ -1,46 +1,48 @@
-#include "telementry.h"
+#include "telemetry.h"
 #include "FreeRTOS.h"
 #include "application/fsm/fsm.h"
 
-#define TELEMETRY_MAX_SOURCES   32 // TODO: find out real value for this
+#define TELEMETRY_MAX_SOURCES 32 // TODO: find out real value for this
 #define TELEMETRY_TASK_FREQUENCY_HZ 1000 // run telemetry task at 1000hz
-#define TELEMETRY_TASK_PERIOD_MS (1 / TELEMETRY_TASK_FREQUENCY_HZ) * 1000;
+#define TELEMETRY_TASK_PERIOD_MS (1 / TELEMETRY_TASK_FREQUENCY_HZ) * 1000 // period in ms for 1000hz
 
 // struct to keep track of registered telemetry sources
 typedef struct {
-    telemetry_source_config_t sources[TELEMETRY_MAX_SOURCES]; // array of registered telemetry sources
-    uint32_t num_sources;
+	telemetry_source_config_t
+		sources[TELEMETRY_MAX_SOURCES]; // array of registered telemetry sources
+	uint32_t num_sources;
 } telemetry_registry_t;
 
 typedef struct {
-    uint16_t failed_transmissions;
-    uint16_t successful_transmissions;
+	uint16_t failed_transmissions;
+	uint16_t successful_transmissions;
 } telemetry_stats_t;
 
 static telemetry_registry_t g_telemetry_registry = {0};
 
-w_status_t telemetry_init(void){
-    // todo: implement telemetry initialization (e.g., setting up CAN, UART, etc.)
-    return W_SUCCESS;
+w_status_t telemetry_init(void) {
+	return W_SUCCESS;
 }
 
-w_status_t telemetry_register(const telemetry_source_config_t *config){
-    return W_SUCCESS;
+w_status_t telemetry_register(const telemetry_source_config_t *config) {
+	return W_SUCCESS;
 }
 
-health_status_t telemetry_get_status(void){
-    health_status_t status = {.error_bitfield = 0, .module_id = MODULE_TELEMETRY, .severity = HEALTH_OK};
+health_status_t telemetry_get_status(void) {
+	health_status_t status = {
+		.error_bitfield = 0, .module_id = MODULE_TELEMETRY, .severity = HEALTH_OK};
 
-    return status;
+	return status;
 }
 
-void telemetry_task(void *argument){
-    xtick_t lastWakeTime = xTaskGetTickCount();
+void telemetry_task(void *argument) {
+	TickType_t lastWakeTime = xTaskGetTickCount();
 
-    while(1){
-        //todo: implement telemetry logging based on registered sources and their desired frequencies
-        vtaskDelayUntil(&lastWakeTime, pdMS_TO_TICKS(TELEMETRY_TASK_PERIOD_MS));
-    }
+	while (1) {
+		// todo: implement telemetry logging based on registered sources and their desired
+		// frequencies
+		vTaskDelayUntil(&lastWakeTime, pdMS_TO_TICKS(TELEMETRY_TASK_PERIOD_MS));
+	}
 
-    return;
+	return;
 }
--- src/application/telemetry/telemetry.h
+++ src/application/telemetry/telemetry.h
@@ -1,6 +1,6 @@
-#include "application/fsm/fsm.h"   // fsm_state_t
-#include "rocketlib/include/common.h"
+#include "application/fsm/fsm.h" // fsm_state_t
 #include "application/health_checks/health_checks.h"
+#include "rocketlib/include/common.h"
 #include <stdbool.h>
 #include <stdint.h>
 
@@ -9,21 +9,21 @@ typedef void (*telemetry_log_fn_t)(void);
 
 // struct for telemetry source configuration
 typedef struct {
-    const char *name;                          // for status/debug logging
+	const char *name; // for status/debug logging
 
-    telemetry_log_fn_t log_fn;                 // function to call periodically to log data
-    uint32_t desired_frequency_hz;
-    fsm_state_t flight_phase_state;
+	telemetry_log_fn_t log_fn; // function to call periodically to log data
+	uint32_t desired_frequency_hz;
+	fsm_state_t flight_phase_state;
 
-    uint32_t period_ms; // the period in ms between logs, derived from desired_frequency_hz
-    uint32_t last_logged_ms; // the last ms timestamp when this source was logged
-    uint32_t due_date_ms; // the next ms timestamp when this source should be logged
+	uint32_t period_ms; // the period in ms between logs, derived from desired_frequency_hz
+	uint32_t last_logged_ms; // the last ms timestamp when this source was logged
+	uint32_t due_date_ms; // the next ms timestamp when this source should be logged
 } telemetry_source_config_t;
 
 // initialize the telemetry module
 w_status_t telemetry_init(void);
 
-// register function 
+// register function
 w_status_t telemetry_register(const telemetry_source_config_t *config);
 
 // rtos task
