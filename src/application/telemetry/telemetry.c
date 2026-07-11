6e8c1c659d877104c952f37f0495bcd393957470
Shiman <167009963+Shiman-Zhu@users.noreply.github.com>
2026-07-11 19:48:51 -0400

    init implementation for telemetry module with logging and error handling

--- src/application/telemetry/telemetry.c
+++ src/application/telemetry/telemetry.c
@@ -1,6 +1,8 @@
 #include "telemetry.h"
 #include "FreeRTOS.h"
 #include "application/fsm/fsm.h"
+#include "application/health_checks/health_checks.h"
+#include "application/logging/logging.h"
 
 #define TELEMETRY_MAX_SOURCES 32 // TODO: find out real value for this
 #define TELEMETRY_TASK_FREQUENCY_HZ 1000 // run telemetry task at 1000hz
@@ -14,17 +16,61 @@ typedef struct {
 } telemetry_registry_t;
 
 typedef struct {
+	bool initialized;
 	uint16_t failed_transmissions;
 	uint16_t successful_transmissions;
 } telemetry_stats_t;
 
 static telemetry_registry_t g_telemetry_registry = {0};
+static telemetry_stats_t g_telemetry_stats = {0};
 
+static void 
 w_status_t telemetry_init(void) {
+	if(g_telemetry_stats.initialized) {
+		log_text(1, LOG_LEVEL_WARN, "telemetry module", "Telemetry module already initialized");
+		return W_FAILURE;
+	}
+
+	g_telemetry_stats.failed_transmissions = 0;
+	g_telemetry_stats.successful_transmissions = 0;
+
+	for(uint32_t i = 0; i < TELEMETRY_MAX_SOURCES; i++) {
+		g_telemetry_registry.sources[i].name = NULL;
+		g_telemetry_registry.sources[i].log_fn = NULL;
+		g_telemetry_registry.sources[i].desired_frequency_hz = 0;
+		g_telemetry_registry.sources[i].flight_phase_state = STATE_ERROR;
+		g_telemetry_registry.sources[i].period_ms = 0;
+		g_telemetry_registry.sources[i].last_logged_ms = 0;
+		g_telemetry_registry.sources[i].due_date_ms = 0;
+	}
+
+	g_telemetry_registry.num_sources = 0;
+	g_telemetry_stats.initialized = true;
+
 	return W_SUCCESS;
 }
 
 w_status_t telemetry_register(const telemetry_source_config_t *config) {
+	if(!g_telemetry_stats.initialized) {
+		log_text(1, LOG_LEVEL_ERROR, "telemetry module", "Telemetry module not initialized");
+		return W_FAILURE;
+	}
+	
+	if (g_telemetry_registry.num_sources >= TELEMETRY_MAX_SOURCES) {
+		log_text(1, LOG_LEVEL_ERROR, "telemetry module", "Maximum number of telemetry sources reached");
+		return W_FAILURE;
+	}
+
+	if(config->name == NULL || config->log_fn == NULL || config->desired_frequency_hz == 0) {
+		log_text(1, LOG_LEVEL_ERROR, "telemetry module", "Invalid telemetry source configuration");
+		return W_INVALID_PARAM;
+	}
+
+	config->period_ms = 1000 / config->desired_frequency_hz; // todo: add a static function to conv hz to ms
+
+	g_telemetry_registry.sources[g_telemetry_registry.num_sources] = *config;
+	g_telemetry_registry.num_sources++;
+
 	return W_SUCCESS;
 }
 
@@ -39,8 +85,7 @@ void telemetry_task(void *argument) {
 	TickType_t lastWakeTime = xTaskGetTickCount();
 
 	while (1) {
-		// todo: implement telemetry logging based on registered sources and their desired
-		// frequencies
+		
 		vTaskDelayUntil(&lastWakeTime, pdMS_TO_TICKS(TELEMETRY_TASK_PERIOD_MS));
 	}
 
