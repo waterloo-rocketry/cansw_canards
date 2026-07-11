e428723fdaddd38b82e9c6f196e7faa77436f2da
Shiman <167009963+Shiman-Zhu@users.noreply.github.com>
2026-07-11 18:20:59 -0400

    telem header fixed and updated with doc!

--- src/application/telemetry/telemetry.h
+++ src/application/telemetry/telemetry.h
@@ -4,23 +4,30 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+// pass in any fuction your module desire to log data
 typedef void (*telemetry_log_fn_t)(void);
 
+// struct for telemetry source configuration
 typedef struct {
     const char *name;                          // for status/debug logging
 
-    telemetry_log_fn_t log_fn;                 // fills container + calls log_data()
+    telemetry_log_fn_t log_fn;                 // function to call periodically to log data
     uint32_t desired_frequency_hz;
     fsm_state_t flight_phase_state;
 
-    uint32_t period_ms;
-    uint32_t due_date_ms; // the rtos tickcount that the callback should get called next
+    uint32_t period_ms; // the period in ms between logs, derived from desired_frequency_hz
+    uint32_t last_logged_ms; // the last ms timestamp when this source was logged
+    uint32_t due_date_ms; // the next ms timestamp when this source should be logged
 } telemetry_source_config_t;
 
+// initialize the telemetry module
 w_status_t telemetry_init(void);
+
 // register function 
-w_status_t telemetry_transmit_hz(const telemetry_source_config_t *config);
+w_status_t telemetry_register(const telemetry_source_config_t *config);
+
 // rtos task
 void telemetry_task(void *argument);
+
 // for health checks
 health_status_t telemetry_get_status(void);
